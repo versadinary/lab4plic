9acdbfed3a03fb0c4f8e5d3cbc01025382f531e2
versadinary <170194434+versadinary@users.noreply.github.com>
2025-04-09 18:24:00 +0300

    Update wrapper.sv

--- wrapper.sv
+++ wrapper.sv
@@ -41,13 +41,13 @@ module wrapper(
     
     PLLE2_BASE #(
       .BANDWIDTH("OPTIMIZED"),  // OPTIMIZED, HIGH, LOW
-      .CLKFBOUT_MULT(2),  // 39       // Multiply value for all CLKOUT, (2-64)
+      .CLKFBOUT_MULT(11),  // 39       // Multiply value for all CLKOUT, (2-64)
       .CLKFBOUT_PHASE(0.0),     // Phase offset in degrees of CLKFB, (-360.000-360.000).
       // Please fix this 
       .CLKIN1_PERIOD(10),      // Input clock period in ns to ps resolution (i.e. 33.333 is 30 MHz).
       
       // CLKOUT0_DIVIDE - CLKOUT5_DIVIDE: Divide amount for each CLKOUT (1-128)
-      .CLKOUT0_DIVIDE(1), // 60
+      .CLKOUT0_DIVIDE(20), // 60
       .CLKOUT1_DIVIDE(1), // 39
       .CLKOUT2_DIVIDE(1),
       .CLKOUT3_DIVIDE(1),
@@ -85,9 +85,9 @@ module wrapper(
       .CLKIN1(CLK100MHZ),     // 1-bit input: Input clock
       // Control Ports: 1-bit (each) input: PLL control ports
       .PWRDWN(SW[7]),     // 1-bit input: Power-down
-      .RST(rst),           // 1-bit input: Reset
+      .RST(~CPU_RESETN),           // 1-bit input: Reset
       // Feedback Clocks: 1-bit (each) input: Clock feedback ports
-      .CLKFBIN(CLKFBIN)    // 1-bit input: Feedback clock
+      .CLKFBIN(CLKFBOUT)    // 1-bit input: Feedback clock
    );
    // assign LED[0] = ~LOCKED;
    logic [33:0] count1;
